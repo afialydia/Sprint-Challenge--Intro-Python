0383555a87687d0a504623474f0821fa59f5cffa
Afia-Lydia <you@example.com>
2020-07-10 13:32:07 -0400

    updated code to pass all tests.

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -55,7 +55,7 @@ cityreader(cities)
 # Print the list of cities (name, lat, lon), 1 record per line.
 for c in cities:
   coOrds = (c.name,c.lat,c.lon)
-  print((c.name,c.lat,c.lon))
+  print(coOrds)
 
 
 print(len(cities))
